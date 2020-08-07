4da795d53964be9cda20da11d93930f631afb235
jaeju.jang <jjj0611@gmail.com>
2020-08-07 23:59:01 +0900

    feat: notification과 watch용 repository 생성

---
+++ src/main/java/org/kiworkshop/blind/notification/model/NotificationRepository.java
@@ -0,0 +1,6 @@
+package org.kiworkshop.blind.notification.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
---
+++ src/main/java/org/kiworkshop/blind/notification/model/WatchRepository.java
@@ -0,0 +1,6 @@
+package org.kiworkshop.blind.notification.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WatchRepository extends JpaRepository<Watch, Long> {
+}
