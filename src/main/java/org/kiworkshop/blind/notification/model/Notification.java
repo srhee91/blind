1afc610868d0fb1c7c709d48d8bb49e4fae9939c
jaeju.jang <jjj0611@gmail.com>
2020-08-07 23:20:59 +0900

    fix: create notification entity

---
+++ src/main/java/org/kiworkshop/blind/notification/model/Notification.java
@@ -0,0 +1,52 @@
+package org.kiworkshop.blind.notification.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.util.Assert;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private Long postId;
+    private Long userId;
+    private String message;
+    private boolean read;
+
+    @Builder
+    private Notification(Long postId, Long userId, String message) {
+        Assert.notNull(postId, "postId should not be null");
+        Assert.notNull(userId, "postId should not be null");
+        Assert.hasLength(message, "message should not be empty");
+
+        this.postId = postId;
+        this.userId = userId;
+        this.message = message;
+        this.read = false;
+    }
+
+    public
+    void readNotification() {
+        read = true;
+    }
+}
---
+++ src/test/java/org/kiworkshop/blind/notification/model/NotificationTest.java
@@ -0,0 +1,45 @@
+package org.kiworkshop.blind.notification.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class NotificationTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"postId", "userId", "message"})
+    void creationException(String field) {
+        assertThatThrownBy(() ->
+            Notification.builder()
+                .postId(field.equals("postId") ? null : 1L)
+                .userId(field.equals("userId") ? null : 1L)
+                .message(field.equals("message") ? "" : "message")
+                .build()
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void readNotification() {
+        Notification notification = getNotificationFixture();
+        notification.readNotification();
+        assertThat(notification.isRead()).isTrue();
+    }
+
+    public static Notification getNotificationFixture() {
+        Notification notification =  Notification.builder()
+            .postId(1L)
+            .userId(1L)
+            .message("1번 게시글이 ... 수정되었습니다.")
+            .build();
+        ReflectionTestUtils.setField(notification, "id", 1L);
+        ReflectionTestUtils.setField(notification, "createdAt", LocalDateTime.now());
+        ReflectionTestUtils.setField(notification, "updatedAt", LocalDateTime.now());
+        return notification;
+    }
+}
