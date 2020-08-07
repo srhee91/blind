2592f35ae268f8a25490527e33139496b4cf2d42
jaeju.jang <jjj0611@gmail.com>
2020-08-08 02:34:20 +0900

    feat: notification 관련 api 추가

---
+++ src/main/java/org/kiworkshop/blind/notification/api/NotificationController.java
@@ -0,0 +1,41 @@
+package org.kiworkshop.blind.notification.api;
+
+import java.util.List;
+
+import org.kiworkshop.blind.notification.model.Notification;
+import org.kiworkshop.blind.notification.service.NotificationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+@RestController
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping
+    public ResponseEntity<List<Notification>> getNotifications(@RequestParam Long userId) {
+        List<Notification> notifications = notificationService.getNotifications(userId);
+        return ResponseEntity.ok(notifications);
+    }
+
+    @PostMapping("/read/{notificationId}")
+    public ResponseEntity<Void> updateNotificationAsRead(@PathVariable Long notificationId, @RequestParam Long userId) {
+        notificationService.readNotification(notificationId, userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<Void> deleteNotification(@PathVariable Long notificationId, @RequestParam Long userId) {
+        notificationService.deleteNotification(notificationId, userId);
+        return ResponseEntity.noContent().build();
+    }
+}
