38a9227d216cd0c560d1f3491e5adf4f54c92da8
jiwoo.kim <jiwoo.kim@deliveryhero.co.kr>
2020-08-14 19:42:15 +0900

    fix: 사용하지 않는 import 삭제

--- src/main/java/org/kiworkshop/blind/notification/model/Watch.java
+++ src/main/java/org/kiworkshop/blind/notification/model/Watch.java
@@ -3,15 +3,12 @@ package org.kiworkshop.blind.notification.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.kiworkshop.blind.post.domain.Post;
